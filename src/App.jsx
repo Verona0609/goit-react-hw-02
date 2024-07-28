fe4dbf01dd1d3b8737aaeca564906240da2866a7
Verona0609 <dikaya.nika15@gmail.com>
2024-07-28 18:51:36 +0200

    home-work 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,73 @@
 import "./App.css";
-import FriendList from "./components/FriendList";
-import Profile from "./components/Profile";
-import TransactionHistory from "./components/TransactionHistory";
-import friends from "./friends.json";
-import userData from "./userData.json";
-import transactions from "./transactions.json";
+import { useEffect, useState } from "react";
+import Description from "./components/Description";
+import Options from "./components/Options";
+import Feedback from "./components/Feedback";
+import Notification from "./components/Notification";
 
 const App = () => {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem("feedback");
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
+  });
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => {
+      const newFeedback = {
+        ...prevFeedback /* шоб зберегти попередні дані  */,
+        [feedbackType]: prevFeedback[feedbackType] + 1,
+      };
+      localStorage.setItem("feedback", JSON.stringify(newFeedback));
+      return newFeedback;
+    });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiv = Math.round(
+    ((feedback.good + feedback.neutral) / totalFeedback) * 100
+  );
+
+  /*   useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  useEffect(() => {
+    const savedFeedback = localStorage.getItem("feedback");
+    if (savedFeedback) {
+      setFeedback(JSON.parse(savedFeedback));
+    }
+  }, []); */
+
+  const resetClick = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
   return (
     <>
-      <Profile
-        username={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
+      <Description />
+      <Options
+        onLeaveFeedback={updateFeedback}
+        onResetClick={resetClick}
+        totalFeedback={totalFeedback}
       />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+
+      {totalFeedback > 0 ? (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={totalFeedback}
+          positiv={positiv}
+        />
+      ) : (
+        <Notification message="No feedback yet" />
+      )}
     </>
   );
 };
